ee605c0fcc8e153ba8b6db2ce0aa473392ba8872
Kazuchan1889 <134587893+Kazuchan1889@users.noreply.github.com>
2025-07-02 15:03:42 +0700

    Update api.jsx

--- src/Components/api.jsx
+++ src/Components/api.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://backend-viking-project-production.up.railway.app/api",
+  baseURL: "http://31.97.66.224:8000/api",
   withCredentials: false,
   headers: {
     Accept: "application/json",
